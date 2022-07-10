b86e81c9c415b45ecedaf29a6ab9aa8cf0831c21
aeof <30540276+dialtcp@users.noreply.github.com>
2022-07-10 17:26:17 +0800

    Feat: implement basic TCP client.

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 3 {
+		log.Fatal("Usage: nc host port")
+	}
+	host := os.Args[1]
+	port := os.Args[2]
+
+	conn, err := net.Dial("tcp", host+":"+port)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	go func() {
+		io.Copy(conn, os.Stdin)
+	}()
+	io.Copy(os.Stdin, conn)
+}
